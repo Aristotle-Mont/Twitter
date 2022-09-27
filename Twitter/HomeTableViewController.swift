53bbd17a1f81ee63511f2d543ce4355fd7feeca4
Phantom <montalvod2020@gmail.com>
2022-09-27 00:02:48 -0400

    Updated files

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -15,6 +15,7 @@ class HomeTableViewController: UITableViewController {
     
     //to refresh page
     let myRefreshControl = UIRefreshControl()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +23,16 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        //two code below are uneeded but just incase
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
     //this for the tweets using the API
     @objc func loadTweets(){
         
@@ -104,6 +113,11 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        //this is from the tweetviewcontroller
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Phantom on 9/23/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    @IBAction func tweet(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error, unable to Tweet! \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    
+}
